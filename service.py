dfbb62686f0c0e49a7448bedf37686139724bc15
Layne Smith <laynesmith@Groots-MacBook-Pro.local>
2019-01-31 13:32:47 -0600

    Update for deploy

--- service.py
+++ service.py
@@ -1,21 +1,21 @@
 from utils import upload_data_s3
 # import the filename of your scraper
-import scraper_file
+import getschedule
 import json
 
 def handler(event, context):
 
     # Call the function from your imported file
-    data = json.dumps(scraper_file.scrapeStuff())
+    data = getschedule.scrapeSchedule()
 
     # Update where the file will be saved
-    bucket_filepath = 'data-store/2018/<file-name>.csv'
+    bucket_filepath = '2018/fusion/fusion-schedule.csv'
 
     # Upload it - JSON
-    upload_data_s3(data, bucket_filepath, 'json')
+    # upload_data_s3(data, bucket_filepath, 'json')
 
     # Upload it - CSV
-    # upload_data_s3(data, bucket_filepath, 'csv')
+    upload_data_s3(data, bucket_filepath, 'csv')
 
 
 # this function is just for our testing purposes,
